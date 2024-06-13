4ffde93f64200bf3f631ce990c671595518b0e59
unknown <nonyeroya100l@gmail.com>
2024-06-13 12:32:18 +0100

    mobile view commit

--- script.js
+++ script.js
@@ -15,27 +15,27 @@ const x = document.querySelectorAll(".hidden");
 x.forEach((el) => observer.observe(el));
 
 
-// swiper slide 
-const slider = new Swiper('.swiper', {
-    pagination: {
-        el: '.swiper-pagination'
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    slidesPerView: 3,
-    spaceBetween: 100,
-    speed: 3000,
-    loop: true,
-    autoplay: {
-    delay:3000,
-    },
-})
+// // swiper slide 
+// const slider = new Swiper('.swiper', {
+//     pagination: {
+//         el: '.swiper-pagination'
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//     slidesPerView: 1,
+//     spaceBetween: 100,
+//     speed: 3000,
+//     loop: true,
+//     autoplay: {
+//     delay:3000,
+//     },
+// })
 
-load = () =>{
+// load = () =>{
   
-}
+// }
 
 document.getElementById('CV').addEventListener('click', function() {
     // URL of the PDF file
@@ -45,6 +45,53 @@ document.getElementById('CV').addEventListener('click', function() {
     // Open the PDF in a new tab
     window.open(pdfUrl, '_blank');
 });
+
+document.getElementById('learnMore').addEventListener('click', function() {
+    // URL of the PDF file
+    
+    var pdf = '/images/dev.pdf';
+    
+    // Open the PDF in a new tab
+    window.open(pdf, '_blank');
+});
+
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('nav');
+
+menu.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+    const sliderTrack = document.querySelector('.slider-track');
+    const slides = document.querySelectorAll('.slide');
+    let currentIndex = 0;
+
+    const updateSliderPosition = () => {
+        const slideWidth = slides[0].clientWidth;
+        const offset = -currentIndex * slideWidth;
+        sliderTrack.style.transform = `translateX(${offset}px)`;
+    };
+
+    prevButton.addEventListener('click', () => {
+        currentIndex = (currentIndex > 0) ? currentIndex - 1 : slides.length - 1;
+        updateSliderPosition();
+    });
+
+    nextButton.addEventListener('click', () => {
+        currentIndex = (currentIndex < slides.length - 1) ? currentIndex + 1 : 0;
+        updateSliderPosition();
+    });
+
+    window.addEventListener('resize', updateSliderPosition);
+    updateSliderPosition(); // Initial call to set position correctly
+});
+
+
+
+
 
 
 // document.getElementById('CV').onclick = function() {
